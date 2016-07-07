0273cc72c788a3a5f477df73e0df14cbef6532fe
Florian Moser <git@famoser.ch>
2016-07-07 13:28:20 +0200

    addint test project

---
+++ Famoser.YoutubeExtractor.Platform.UniversalApplication.Tests/UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Famoser.YoutubeExtractor.Platform.UniversalApplication.Tests
+{
+    [TestClass]
+    public class TestAudioConversion
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            //IRandomAccessStream readStream = await file.OpenAsync(FileAccessMode.Read);
+            //FFmpegMSS = FFmpegInteropMSS.CreateFFmpegInteropMSSFromStream(readStream, false, false);
+            //// Pass MediaStreamSource to Media Element
+            //mediaElement.SetMediaStreamSource(FFmpegMSS.GetMediaStreamSource());
+        }
+    }
+}
